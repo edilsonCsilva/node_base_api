9da11be833c0e8f375e4e667f4eb1c0bffaf4238
edi <edi@CasaVt>
2021-01-09 13:22:53 -0300

    loads

---
+++ config/express.js
@@ -0,0 +1,17 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const config     = require('config');
+
+module.exports = () => {
+  const app = express();
+
+  // SETANDO VARIÁVEIS DA APLICAÇÃO
+ // app.set('port', process.env.PORT || config.get('server.port'));
+
+ app.set('port',8080);
+
+  // MIDDLEWARES
+  app.use(bodyParser.json());
+
+  return app;
+};
